47927efcbcde67316169c06eb3b0af7f86840498
georges kaputu <gkaputu@bmlcenter.com>
2024-02-07 17:14:43 +0100

    add complet

--- core/settings.py
+++ core/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 
-STATIC_URL = "static"
+STATIC_URL = "static/"
 
 
 STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
@@ -131,7 +131,7 @@ STATICFILES_DIRS = [
 
 
 
-MEDIA_URL = 'media'
+MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
